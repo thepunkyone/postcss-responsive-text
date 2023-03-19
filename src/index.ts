2528bdd3084019acdfa8a1a63b38a2e2fb20c68a
Whitney Bluzma <thepunkyone@gmail.com>
2023-03-19 22:33:44 +0000

    refactor: rename rules and variables to be more clear

--- src/index.ts
+++ src/index.ts
@@ -9,56 +9,58 @@ import { AtRule, Result, Root, Rule } from 'postcss'
 const postcss = require('postcss')
 
 const DEFAULT_PARAMS = {
-    'font-size': {
-      minSize: '12px',
-      maxSize: '21px',
-      minWidth: '420px',
-      maxWidth: '1280px',
-    },
-    'line-height': {
-      minSize: '1.2em',
-      maxSize: '1.8em',
-      minWidth: '420px',
-      maxWidth: '1280px',
-    },
-    'letter-spacing': {
-      minSize: '0px',
-      maxSize: '4px',
-      minWidth: '420px',
-      maxWidth: '1280px',
-    },
+  'font-size': {
+    minSize: '12px',
+    maxSize: '21px',
+    minWidth: '420px',
+    maxWidth: '1280px',
   },
-  PARAM_RANGE = {
-    'font-size': 'font-range' as const,
-    'line-height': 'line-height-range' as const,
-    'letter-spacing': 'letter-spacing-range' as const,
+  'line-height': {
+    minSize: '1.2em',
+    maxSize: '1.8em',
+    minWidth: '420px',
+    maxWidth: '1280px',
   },
-  PARAM_DECLS = {
-    'font-size': {
-      minSize: 'min-font-size' as const,
-      maxSize: 'max-font-size' as const,
-      minWidth: 'lower-font-range' as const,
-      maxWidth: 'upper-font-range' as const,
-    },
-    'line-height': {
-      minSize: 'min-line-height' as const,
-      maxSize: 'max-line-height' as const,
-      minWidth: 'lower-line-height-range' as const,
-      maxWidth: 'upper-line-height-range' as const,
-    },
-    'letter-spacing': {
-      minSize: 'min-letter-spacing' as const,
-      maxSize: 'max-letter-spacing' as const,
-      minWidth: 'lower-letter-spacing-range' as const,
-      maxWidth: 'upper-letter-spacing-range' as const,
-    },
-  }
+  'letter-spacing': {
+    minSize: '0px',
+    maxSize: '4px',
+    minWidth: '420px',
+    maxWidth: '1280px',
+  },
+}
 
-type DeclName = keyof typeof DEFAULT_PARAMS
-type DeclNameParamRange = (typeof PARAM_RANGE)[keyof typeof PARAM_RANGE]
-type RangeDecl = (typeof PARAM_DECLS)[keyof typeof PARAM_DECLS]
+const PARAM_RANGES = {
+  'font-size': 'font-range' as const,
+  'line-height': 'line-height-range' as const,
+  'letter-spacing': 'letter-spacing-range' as const,
+}
+
+const CUSTOM_PARAMS = {
+  'font-size': {
+    minSize: 'min-font-size' as const,
+    maxSize: 'max-font-size' as const,
+    minWidth: 'lower-font-range' as const,
+    maxWidth: 'upper-font-range' as const,
+  },
+  'line-height': {
+    minSize: 'min-line-height' as const,
+    maxSize: 'max-line-height' as const,
+    minWidth: 'lower-line-height-range' as const,
+    maxWidth: 'upper-line-height-range' as const,
+  },
+  'letter-spacing': {
+    minSize: 'min-letter-spacing' as const,
+    maxSize: 'max-letter-spacing' as const,
+    minWidth: 'lower-letter-spacing-range' as const,
+    maxWidth: 'upper-letter-spacing-range' as const,
+  },
+}
+
+type CssAttribute = keyof typeof DEFAULT_PARAMS
+type CustomRange = (typeof PARAM_RANGES)[keyof typeof PARAM_RANGES]
+type CustomAttributes = (typeof CUSTOM_PARAMS)[keyof typeof CUSTOM_PARAMS]
 type DefaultParams = (typeof DEFAULT_PARAMS)[keyof typeof DEFAULT_PARAMS]
-type Rules = {
+type CustomRules = {
   responsive: string
   minMedia: AtRule
   maxMedia: AtRule
@@ -87,20 +89,20 @@ function getUnit(value: string): string | null {
  * @return {String} rem value
  */
 function pxToRem(px: string): string {
-  return parseFloat(px) / parseFloat(rootSize) + 'rem'
+  return `${parseFloat(px) / parseFloat(rootSize)}rem`
 }
 
 function fetchResponsiveSizes(
   rule: Rule,
-  declName: DeclName,
+  declarationName: CssAttribute,
   cb: (minSize: string, maxSize: string) => void
 ): void {
-  rule.walkDecls(declName, (decl) => {
-    if (decl.value.indexOf('responsive') > -1) {
-      const foundSizes = decl.value.match(/-?\d*\.?\d+(?:\w+)?/g)
+  rule.walkDecls(declarationName, (declaration) => {
+    if (declaration.value.indexOf('responsive') > -1) {
+      const sizes = declaration.value.match(/-?\d*\.?\d+(?:\w+)?/g)
 
-      if (foundSizes) {
-        cb(foundSizes[0], foundSizes[1])
+      if (sizes) {
+        cb(sizes[0], sizes[1])
       }
     }
   })
@@ -108,41 +110,41 @@ function fetchResponsiveSizes(
 
 function fetchRangeSizes(
   rule: Rule,
-  declName: DeclNameParamRange,
+  declarationName: CustomRange,
   cb: (minSize: string, maxSize: string) => void
 ) {
-  rule.walkDecls(declName, (decl) => {
-    const sizes = decl.value.split(/\s+/)
+  rule.walkDecls(declarationName, (declaration) => {
+    const sizes = declaration.value.split(/\s+/)
 
     cb(sizes[0], sizes[1])
-    decl.remove()
+    declaration.remove()
   })
 }
 
-function fetchParams(rule: Rule, declName: DeclName) {
-  const params = Object.assign({}, DEFAULT_PARAMS[declName])
+function getParams(rule: Rule, declarationName: CssAttribute) {
+  const params = Object.assign({}, DEFAULT_PARAMS[declarationName])
 
-  // Fetch params from shorthand declName, i.e., font-size or line-height, etc
-  fetchResponsiveSizes(rule, declName, (minSize, maxSize) => {
+  // Fetch params from shorthand declarationName, i.e., font-size or line-height, etc
+  fetchResponsiveSizes(rule, declarationName, (minSize, maxSize) => {
     params.minSize = minSize
     params.maxSize = maxSize
   })
 
   // Fetch params from shorthand font-range or line-height-range
-  fetchRangeSizes(rule, PARAM_RANGE[declName], (minSize, maxSize) => {
+  fetchRangeSizes(rule, PARAM_RANGES[declarationName], (minSize, maxSize) => {
     params.minWidth = minSize
     params.maxWidth = maxSize
   })
 
   // Fetch parameters from expanded properties
-  const rangeDecl: RangeDecl = PARAM_DECLS[declName]
+  const customAttributes: CustomAttributes = CUSTOM_PARAMS[declarationName]
 
-  let key: keyof typeof rangeDecl
+  let attribute: keyof typeof customAttributes
 
-  for (key in rangeDecl) {
-    rule.walkDecls(rangeDecl[key], (decl) => {
-      params[key] = decl.value.trim()
-      decl.remove()
+  for (attribute in customAttributes) {
+    rule.walkDecls(customAttributes[attribute], (declaration) => {
+      params[attribute] = declaration.value.trim()
+      declaration.remove()
     })
   }
 
@@ -153,29 +155,32 @@ function isRule(node: unknown): node is Rule {
   return (<{ type?: unknown }>node).type === 'rule'
 }
 
-function isDeclName(declProp: string): declProp is DeclName {
-  return Object.keys(DEFAULT_PARAMS).includes(declProp)
+function isCssAttribute(attribute: string): attribute is CssAttribute {
+  return Object.keys(DEFAULT_PARAMS).includes(attribute)
 }
 
 /**
  * Build new responsive type rules
  * @param  {object} rule     old CSS rule
- * @param declName
+ * @param declarationName
  * @param params
  * @param result
  * @return {object}          object of new CSS rules
  */
-function buildRules(rule: Rule, declName: DeclName, params: DefaultParams, result: Result) {
-  let minSize = params.minSize,
-    maxSize = params.maxSize,
-    minWidth,
-    maxWidth,
-    sizeUnit = getUnit(params.minSize),
-    maxSizeUnit = getUnit(params.maxSize),
-    widthUnit = getUnit(params.minWidth),
-    maxWidthUnit = getUnit(params.maxWidth),
-    sizeDiff,
-    rangeDiff
+function buildRules(
+  rule: Rule,
+  declarationName: CssAttribute,
+  params: DefaultParams,
+  result: Result
+): CustomRules {
+  let minWidth
+  let maxWidth
+  const minSize = params.minSize
+  const maxSize = params.maxSize
+  const sizeUnit = getUnit(params.minSize)
+  const maxSizeUnit = getUnit(params.maxSize)
+  const widthUnit = getUnit(params.minWidth)
+  const maxWidthUnit = getUnit(params.maxWidth)
 
   if (sizeUnit === null) {
     throw rule.error('sizes with unitless values are not supported')
@@ -196,20 +201,19 @@ function buildRules(rule: Rule, declName: DeclName, params: DefaultParams, resul
   }
 
   // Build the responsive type declaration
-  sizeDiff = parseFloat(maxSize) - parseFloat(minSize)
-  rangeDiff = parseFloat(<string>maxWidth) - parseFloat(<string>minWidth)
+  const sizeDifference = parseFloat(maxSize) - parseFloat(minSize)
+  const rangeDifference = parseFloat(<string>maxWidth) - parseFloat(<string>minWidth)
 
-  let rules: Rules = {
-    responsive:
-      'calc(' + minSize + ' + ' + sizeDiff + ' * ((100vw - ' + minWidth + ') / ' + rangeDiff + '))',
+  const rules: CustomRules = {
+    responsive: `calc(${minSize} + ${sizeDifference} * ((100vw - ${minWidth}) / ${rangeDifference}))`,
     // Build the media queries
     minMedia: postcss.atRule({
       name: 'media',
-      params: 'screen and (max-width: ' + params.minWidth + ')',
+      params: `screen and (max-width: ${params.minWidth})`,
     }),
     maxMedia: postcss.atRule({
       name: 'media',
-      params: 'screen and (min-width: ' + params.maxWidth + ')',
+      params: `screen and (min-width: ${params.maxWidth})`,
     }),
   }
 
@@ -220,7 +224,7 @@ function buildRules(rule: Rule, declName: DeclName, params: DefaultParams, resul
     })
     .walkRules((selector) => {
       selector.append({
-        prop: declName,
+        prop: declarationName,
         value: params.minSize,
       })
     })
@@ -231,7 +235,7 @@ function buildRules(rule: Rule, declName: DeclName, params: DefaultParams, resul
     })
     .walkRules((selector) => {
       selector.append({
-        prop: declName,
+        prop: declarationName,
         value: params.maxSize,
       })
     })
@@ -242,36 +246,39 @@ function buildRules(rule: Rule, declName: DeclName, params: DefaultParams, resul
 module.exports = () => {
   return {
     postcssPlugin: 'postcss-responsive-text',
-    Once(root: Root, { result }: { result: Result }) {
+    Once(root: Root, { result }: { result: Result }): void {
       root.walkRules(function (rule) {
         // Check root font-size (for rem units)
         if (rule.selector.indexOf('html') > -1) {
-          rule.walkDecls('font-size', (decl) => {
-            if (decl.value.indexOf('px') > -1) {
-              rootSize = decl.value
+          rule.walkDecls('font-size', (declaration) => {
+            if (declaration.value.indexOf('px') > -1) {
+              rootSize = declaration.value
             }
           })
         }
 
-        rule.walkDecls(/^(font-size|line-height|letter-spacing)$/, (decl) => {
-          // If decl doesn't contain responsive keyword, exit
-          if (decl.value.indexOf('responsive') === -1) {
+        rule.walkDecls(/^(font-size|line-height|letter-spacing)$/, (declaration) => {
+          // If declaration doesn't contain responsive keyword, exit
+          if (declaration.value.indexOf('responsive') === -1) {
             return
           }
 
-          const parentNode = decl.parent
+          const parentNode = declaration.parent
 
-          if (!isRule(parentNode) || !isDeclName(decl.prop)) {
+          // If parent node is not of Rule type or declaration prop is not a valid CSS attribute, exit
+          if (!isRule(parentNode) || !isCssAttribute(declaration.prop)) {
             return
           }
 
-          const params = fetchParams(parentNode, decl.prop)
+          const declarationName = declaration.prop
 
-          const newRules = buildRules(parentNode, decl.prop, params, result)
+          const params = getParams(parentNode, declarationName)
+
+          const newRules = buildRules(parentNode, declarationName, params, result)
 
           // Insert the base responsive declaration
-          if (decl.value.indexOf('responsive') > -1) {
-            decl.replaceWith({ prop: decl.prop, value: newRules.responsive })
+          if (declaration.value.indexOf('responsive') > -1) {
+            declaration.replaceWith({ prop: declarationName, value: newRules.responsive })
           }
 
           // Insert the media queries
